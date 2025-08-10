6c26412b6b2b4656371be8aac5db460f64f399e9
NARAYANAGARIVARSHITHA <153912751+NARAYANAGARIVARSHITHA@users.noreply.github.com>
2025-08-10 21:41:57 +0530

    Update Top 2.v

--- Top 2.v
+++ Top 2.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/26/2023 11:27:52 PM
-// Design Name: 
-// Module Name: Top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module Top(
